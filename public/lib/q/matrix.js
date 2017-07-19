5e5f6a48a1738d65ac6133394632f34d0697115f
Dirichi Ike-Njoku <din206@nyu.edu>
2017-07-20 00:46:14 +0100

    add matrix products

--- public/lib/q/matrix.js
+++ public/lib/q/matrix.js
@@ -26,6 +26,82 @@ class Matrix{
     return array;
   };
 
+  static product(matrix, otherMatrix){
+    Matrix.checkCompatibilityForProduct(matrix, otherMatrix);
+    var productDimension = [matrix.numRows, otherMatrix.numColumns]
+
+    var product = new Matrix(productDimension[0], productDimension[1]);
+
+    for (var i = 0; i < productDimension[0]; i++) {
+      for (var j = 0; j < productDimension[1]; j++) {
+        var row = matrix.row(i);
+        var column = otherMatrix.column(j)
+        product.set(i, j, Matrix.dotProduct(row, column))
+      }
+    }
+
+    return product;
+  }
+
+  static dotProduct(array1, array2){
+    Matrix.checkCompatibilityForDotProduct(array1, array2)
+    var sum = 0
+    for (var i = 0; i < array1.length; i++) {
+      sum += array1[i] * array2[i]
+    }
+
+    return sum;
+  }
+
+
+  static checkCompatibilityForProduct(matrix, otherMatrix){
+    var compatible = matrix.numRows == otherMatrix.numColumns
+    && matrix.numColumns == otherMatrix.numRows
+    if (!compatible) {
+      throw new Error('Matrices not compatible for multiplication')
+    }
+  }
+
+  static checkCompatibilityForDotProduct(array1, array2){
+    var compatible = array1.length == array2.length
+    if (!compatible) {
+      throw new Error('arrays not compatible for dotProduct')
+    }
+  }
+
+  dimensions(){
+    return [this.numRows, this.numColumns]
+  }
+
+  row(index){
+    //should raise error if out of range
+    this.checkOutOfRowRange(index)
+    return this.body[index]
+  }
+
+  checkOutOfRowRange(index){
+    var outOfRange = this.numRows <= index
+    if (outOfRange) {
+      throw new Error('index is out of row range')
+    }
+  }
+
+  column(index){
+    this.checkOutOfColumnRange()
+    var arr = [];
+    this.body.forEach(function (row) {
+      arr.push(row[index])
+    });
+    return arr;
+  }
+
+  checkOutOfColumnRange(index){
+    var outOfRange = this.numColumns <= index
+    if (outOfRange) {
+      throw new Error('index is out of column range')
+    }
+  }
+
   setBody(newBody){
     if (!Array.isArray(newBody)) {
       throw new Error(newBody + ' is not an array');
